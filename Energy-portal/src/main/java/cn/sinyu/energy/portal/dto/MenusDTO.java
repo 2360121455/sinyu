319aaadc401a3a53ce0952624af5183e86cef373
2360121455 <2360121455@qq.com>
2022-04-16 09:54:42 +0800

    能源数据采集与管控平台WEB端

---
+++ Energy-portal/src/main/java/cn/sinyu/energy/portal/dto/MenusDTO.java
@@ -0,0 +1,8 @@
+package cn.sinyu.energy.portal.dto;
+
+import java.io.Serializable;
+
+public class MenusDTO implements Serializable {
+    private String menuName;
+    private String menuLevel;
+}
--- Energy-portal/src/main/resources/static/js/menu.js
+++ Energy-portal/src/main/resources/static/js/menu.js
@@ -7,5 +7,18 @@ let menusApp = new Vue({
             {id: 3,name:'mybatis'},
             {id: 4,name:'springboot'}
         ]
+    },
+    methods: {
+        loadMenus: function () {
+            $.ajax({
+                url: '/portal/menus',
+                success:function (r){
+                    menusApp.menus = r.data
+                }
+            })
+        }
+    },
+    created: function () {
+        this.loadMenus();
     }
 });
